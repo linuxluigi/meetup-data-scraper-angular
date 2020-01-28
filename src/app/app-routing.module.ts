124f4a4adee098eaf40b709f5e47ec4e2abf931e
Steffen Exler <steffen.exler@pm.me>
2020-01-28 16:04:16 +0100

    bugfix: routing url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { SearchPageComponent } from './search-page/search-page.component';
+import { ImprintPageComponent } from './imprint-page/imprint-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
-import { ImprintPageComponent } from './imprint-page/imprint-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'search', component: SearchPageComponent },
   { path: 'imprint', component: ImprintPageComponent },
-  { path: 'imprint', component: PrivacyPageComponent },
+  { path: 'privacy', component: PrivacyPageComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
